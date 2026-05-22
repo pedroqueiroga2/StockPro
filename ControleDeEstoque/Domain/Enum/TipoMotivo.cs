1a4f8f7c21f7af935f16f2e20fdf667b7c538e1f
Pedro Gabriel <pedro.barbosa@objetivasolucao.com.br>
2026-05-22 14:17:35 -0300

    Feat: criando migraiton e Enum

---
+++ ControleDeEstoque/Domain/Enum/TipoMotivo.cs
@@ -0,0 +1,7 @@
+﻿namespace ControleDeEstoque.Domain.Enum;
+
+public enum TipoMotivo
+{
+Entrada = 1,
+Sida = 2
+}
---
+++ ControleDeEstoque/Migrations/20260522171059_AdicionandoEnum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleDeEstoque.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "tipoMotivo",
+                table: "cadProdutos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tipoMotivo",
+                table: "cadProdutos");
+        }
+    }
+}
